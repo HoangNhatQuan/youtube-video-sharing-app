3b22e280167cbd1477097fb25d0ccd29e63fe8c1
HoangNhatQuan <98334389+HoangNhatQuan@users.noreply.github.com>
2024-09-05 02:10:13 +0700

    chore: fix build

--- server/src/modules/notifications/notification.service.ts
+++ server/src/modules/notifications/notification.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
 import { Model } from 'mongoose'
 
-import { Notification } from './entities/Notification.entity'
+import { Notification } from './entities/notification.entity'
 import { MIN_OFFSET } from 'pipelines/offset.pipe'
 import { MAX_LIMIT } from 'pipelines/limit.pipe'
 import { CreateNotificationDto } from './dto/create-noti.dto'
