48ca930ed04aaf0140c38c24726b214cea8f772a
HoangNhatQuan <98334389+HoangNhatQuan@users.noreply.github.com>
2024-09-05 13:16:17 +0700

    remove job

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@nestjs/common'
-import { Interval } from '@nestjs/schedule';
+
 @Injectable()
 export class AppService {
 
-  @Interval(120000)
   getHello(): string {
     console.log('Hello World!')
     return 'Success'
