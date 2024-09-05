8d95244629868b2a9a00925fac5f0b038195aa47
HoangNhatQuan <98334389+HoangNhatQuan@users.noreply.github.com>
2024-09-05 08:46:47 +0700

    chore

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common'
-
+import { Cron } from '@nestjs/schedule';
 @Injectable()
 export class AppService {
+
+    @Cron('*/15 * * * *')
   getHello(): string {
     return 'Hello World!'
   }
